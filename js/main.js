bc90eef85d62360abf062c6d0d4017bd143c358e
saniag081 <sgonzalezhernandez081@gmail.com>
2020-09-07 20:18:19 -0500

    add validate

--- js/main.js
+++ js/main.js
@@ -1,7 +1,34 @@
 const compose = (...functions) => data =>
   functions.reduceRight((value, func) => func(value), data)
 
+let $btn = document.querySelector('#btn');
 let description = $('#description');
 let calories = $('#calories');
-let carbs = $('carbs');
-let protein = $('protein');
+let carbs = $('#carbs');
+let protein = $('#protein');
+
+$btn.addEventListener('click', validateInputs);
+
+description.keypress(() => {
+  description.removeClass('is-invalid');
+})
+calories.keypress(() => {
+  calories.removeClass('is-invalid');
+})
+carbs.keypress(() => {
+  carbs.removeClass('is-invalid');
+})
+protein.keypress(() => {
+  protein.removeClass('is-invalid');
+})
+
+function validateInputs() {
+  description.val() ? '' : description.addClass('is-invalid');
+  calories.val() ? '' : calories.addClass('is-invalid');
+  carbs.val() ? '' : carbs.addClass('is-invalid');
+  protein.val() ? '' : protein.addClass('is-invalid');
+  if (description.val() &&
+    calories.val() &&
+    carbs.val() &&
+    protein.val()) console.log('OK')
+}
